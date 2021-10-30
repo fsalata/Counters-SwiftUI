f8326e98fc1f22bcd9213f60860ab9accac44c9f
Fábio Salata <s2it_fsalata@uolinc.com>
2021-10-30 14:13:48 -0300

    Main project added

---
+++ Counters/CountersApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountersApp.swift
+//  Counters
+//
+//  Created by Fabio Cezar Salata on 30/10/21.
+//
+
+import SwiftUI
+
+@main
+struct CountersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# Counters-SwiftUI
+# Counters
+
+## Install and start the server
+
+```
+$ npm install
+$ npm start
+```
